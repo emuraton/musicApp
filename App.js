43523826185b83712c6ad29d261b7bc3a6b05f95
Emilien Muraton <emilien.muraton@gmail.com>
2019-06-03 14:18:17 +0100

    add title component

--- App.js
+++ App.js
@@ -6,25 +6,20 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
+import Title from './components/Title';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Fragment>
+        <View style={styles.container}>
+          <Title artist="Orelsan" title="San" />
+        </View>
+      </Fragment>
     );
   }
 }
@@ -35,15 +30,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
   }
 });
