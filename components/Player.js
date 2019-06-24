2d684b05d226f7c1f214d61a41222c1b3a66f5a9
Emilien Muraton <emilien.muraton@gmail.com>
2019-06-24 12:20:00 +0100

    pause / launch animation onPress

--- components/Player.js
+++ components/Player.js
@@ -11,73 +11,96 @@ import Icon from 'react-native-vector-icons/Entypo';
 
 const srcImage = require('../assets/san.jpeg');
 
-export default function Player() {
-  let rotateThumbnail = new Animated.Value(0);
-
-  playMusic = () => {
-    rotateThumbnail.setValue(0);
-    Animated.timing(rotateThumbnail, {
-      toValue: 1,
-      duration: 8000,
-      easing: Easing.linear,
-      useNativeDriver: true,
-    }).start(o => {
-      if (o.finished) playMusic();
-    });
+export default class Player extends React.Component {
+  state = {
+    playing: true,
   };
+  rotateThumbnail = new Animated.Value(0);
 
-  playMusic();
-
-  const spin = rotateThumbnail.interpolate({
+  spin = this.rotateThumbnail.interpolate({
     inputRange: [0, 1],
     outputRange: ['0deg', '360deg'],
   });
 
-  const rotateStyle = { transform: [{ rotate: spin }] };
+  rotateStyle = {
+    transform: [{ rotate: this.spin }],
+  };
 
-  return (
-    <View style={styles.container}>
-      <Svg
-        width={200}
-        height={200}
-        style={{
-          position: 'absolute',
-          zIndex: 99,
-        }}
-      >
-        <Path
-          d={`
+  componentWillMount() {
+    this.playMusic();
+  }
+
+  playMusic = value => {
+    Animated.timing(this.rotateThumbnail, {
+      toValue: 1,
+      duration: 8000,
+      easing: Easing.linear,
+      // useNativeDriver: true,
+    }).start(o => {
+      if (o.finished) {
+        this.rotateThumbnail.setValue(0);
+        this.playMusic();
+      }
+    });
+  };
+
+  onPress = () => {
+    this.state.playing
+      ? this.rotateThumbnail.stopAnimation(value => {
+          this.rotateThumbnail.setValue(value);
+        })
+      : this.playMusic(0);
+
+    this.setState(state => ({
+      playing: !state.playing,
+    }));
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.onPress} style={styles.container}>
+        <View>
+          <Svg
+            width={200}
+            height={200}
+            style={{
+              position: 'absolute',
+              zIndex: 99,
+            }}
+          >
+            <Path
+              d={`
             M 0, 100
             a 10,10 0 1,0 198,0
             a 10,10 0 1,0 -198,0
           `}
-          fill="transparent"
-          stroke="white"
-          strokeWidth={2}
-        />
-        <Path
-          d={`
+              fill="transparent"
+              stroke="white"
+              strokeWidth={2}
+            />
+            <Path
+              d={`
             M 0, 100
             a 100,100 0 0,1 100, -100
         `}
-          fill="transparent"
-          stroke="red"
-          strokeWidth={2}
-          strokeDasharray="100, 800"
-        />
-      </Svg>
-      <TouchableOpacity onPress={playMusic}>
-        <Animated.Image
-          style={[styles.thumbnail, rotateStyle]}
-          resizeMode="contain"
-          source={srcImage}
-        />
+              fill="transparent"
+              stroke="red"
+              strokeWidth={2}
+              strokeDasharray="100, 800"
+            />
+          </Svg>
+          <Animated.Image
+            style={[styles.thumbnail, this.rotateStyle]}
+            resizeMode="contain"
+            source={srcImage}
+          />
+        </View>
+        <View style={styles.playButton}>
+          <Icon name="controller-play" color="white" size={40} />
+        </View>
       </TouchableOpacity>
-      <View style={styles.playButton}>
-        <Icon name="controller-play" color="white" size={40} />
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
